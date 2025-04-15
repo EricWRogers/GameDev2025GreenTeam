5e850892c1e50c61720c4c6772db8932f4ff2fb4
JamesonFoster <jamesonbfoster@gmail.com>
2025-04-15 10:25:46 -0500

    Added Statue
    Statue Can Chase

---
+++ Assets/Scripts/StatueAiI.cs
@@ -0,0 +1,84 @@
+using UnityEngine;
+using System.Collections;
+using UnityEngine.AI;
+
+public class StatueAiI : MonoBehaviour
+{
+    public NavMeshAgent navAgent;
+    public Transform player;
+    public LayerMask groundLayer, playerLayer;
+    public float walkPointRange;
+    public float timeBetweenAttacks;
+    public float sightRange;
+    public float attackRange;
+
+    private Vector3 walkPoint;
+    private bool walkPointSet;
+
+    private void Awake()
+    {
+        player = GameObject.Find("Player").transform;
+        navAgent = GetComponent<NavMeshAgent>();
+    }
+
+    private void Update()
+    {
+        bool playerInSightRange = Physics.CheckSphere(transform.position, sightRange, playerLayer);
+        bool playerInAttackRange = Physics.CheckSphere(transform.position, attackRange, playerLayer);
+
+        if (!playerInSightRange && !playerInAttackRange)
+        {
+            Patroling();
+        }
+        else if (playerInSightRange && !playerInAttackRange)
+        {
+            ChasePlayer();
+        }
+    }
+
+    private void Patroling()
+    {
+        if (!walkPointSet)
+        {
+            SearchWalkPoint();
+        }
+
+        if (walkPointSet)
+        {
+            navAgent.SetDestination(walkPoint);
+        }
+
+        Vector3 distanceToWalkPoint = transform.position - walkPoint;
+
+        if (distanceToWalkPoint.magnitude < 1f)
+        {
+            walkPointSet = false;
+        }
+    }
+
+    private void SearchWalkPoint()
+    {
+        float randomZ = Random.Range(-walkPointRange, walkPointRange);
+        float randomX = Random.Range(-walkPointRange, walkPointRange);
+        walkPoint = new Vector3(transform.position.x + randomX, transform.position.y, transform.position.z + randomZ);
+
+        if (Physics.Raycast(walkPoint, -transform.up, 2f, groundLayer))
+        {
+            walkPointSet = true;
+        }
+    }
+
+    private void ChasePlayer()
+    {
+        navAgent.SetDestination(player.position);
+        navAgent.isStopped = false; // Add this line
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, attackRange);
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, sightRange);
+    }
+}
