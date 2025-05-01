c905d3d285840dab7502a9e2d1ae3c13aa811964
JamesonFoster <jamesonbfoster@gmail.com>
2025-05-01 10:20:12 -0500

    Added Deactivate

--- Assets/Scripts/EventActivator.cs
+++ Assets/Scripts/EventActivator.cs
@@ -3,9 +3,17 @@ using UnityEngine;
 public class EventActivator : MonoBehaviour
 {
     public GameObject targetObject;
+    public bool deactivatorInstead = false;
     private void OnTriggerEnter(Collider other)
     {
-        targetObject.SetActive(true);
+        if (deactivatorInstead != true)
+        {
+            targetObject.SetActive(true);
+        }
+        else
+        {
+            targetObject.SetActive(false);
+        }
         gameObject.SetActive(false);
     }
 }
